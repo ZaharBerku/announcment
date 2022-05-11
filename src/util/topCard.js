47ac712576a4bcbb85fff447cd2462ad2ec4468d
Nazar <zaharberku@gmail.com>
2022-05-11 20:14:47 +0300

    refactor

--- src/util/topCard.js
+++ src/util/topCard.js
@@ -1,20 +1,30 @@
-export const topCard = (arr) =>{
-    const arrName = arr.map(element=> element.name.split(" ")).flat().reduce((item,element)=> {
-        item[element] = (item[element] || 0) + 1;
+export const topCard = (arr) => {
+    const arrName = arr.map(element => {
+        const { name, description } = element;
+
+        return `${name.split(" ")} ${description.split(" ")}`.replace(/[^a-zа-яё\s]/gi, " ");
+
+    }).join("").split(" ").reduce((item, element) => {
+        
+        if(element !== ""){
+            item[element] = (item[element] || 0) + 1;
+        }
         return item;
-    },{});
+    }, {});
+
     const newArr = [];
 
-    const [ topWord ] = Object.entries(arrName).sort(([,a],[,b])=> b - a)[0];
+    const [topWord] = Object.entries(arrName).sort(([, a], [, b]) => b - a)[0];
 
     const result = arr.filter(element => {
-        const bool = element.name.split(" ").includes(topWord);
-        if(bool){
+        const { name, description } = element;
+        const bool = name.split(" ").includes(topWord) || description.includes(topWord);
+        if (bool) {
             return element;
-        }else{
+        } else {
             newArr.push(element);
         }
     });
-
+  
     return [...result, ...newArr];
 };
