b2a991a84f5aaa0a2bee06caa6f091e4bd39d70c
Nazar <zaharberku@gmail.com>
2022-05-13 20:22:29 +0300

    space

--- src/util/topCard.js
+++ src/util/topCard.js
@@ -4,21 +4,20 @@ export const topCard = (arr) => {
 
         return `${name.split(" ")} ${description.split(" ")}`.replace(/[^a-zа-яё\s]/gi, " ");
 
-    }).join("").split(" ").reduce((item, element) => {
-        
-        if(element !== ""){
-            item[element] = (item[element] || 0) + 1;
-        }
-        return item;
-    }, {});
-
+    }).join(" ").split(" ").reduce((item, element) => {
+            if(element !== ""){
+                item[element] = (item[element] || 0) + 1;
+            }
+            return item;
+        }, {});
+    
     const newArr = [];
 
     const [topWord] = Object.entries(arrName).sort(([, a], [, b]) => b - a)[0];
 
     const result = arr.filter(element => {
         const { name, description } = element;
-        const bool = name.split(" ").includes(topWord) || description.includes(topWord);
+        const bool = name.split(" ").includes(topWord) || description.split(" ").includes(topWord);
         if (bool) {
             return element;
         } else {
